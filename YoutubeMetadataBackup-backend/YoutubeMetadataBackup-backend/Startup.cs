7bb38c3d75e497b04380d953961a78796c15ba8b
Shangqing Zong <yin2690855381@gmail.com>
2021-04-21 08:58:13 -0500

    Added support for reverse proxy hosting

--- YoutubeMetadataBackup-backend/YoutubeMetadataBackup-backend/Startup.cs
+++ YoutubeMetadataBackup-backend/YoutubeMetadataBackup-backend/Startup.cs
@@ -1,5 +1,7 @@
+using System.Net;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -22,6 +24,12 @@ namespace YoutubeMetadataBackup_backend
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Uncomment below section if needed
+            //services.Configure<ForwardedHeadersOptions>(options =>
+            //{
+            //    options.KnownProxies.Add(IPAddress.Parse("10.0.0.100"));
+            //});
+
             services.AddCors(options =>
             {
                 options.AddPolicy("AllowAll",
@@ -52,6 +60,12 @@ namespace YoutubeMetadataBackup_backend
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "YoutubeMetadataBackup_backend v1"));
             }
+
+            // For Deploy with Nginx and other reverse proxy services
+            app.UseForwardedHeaders(new ForwardedHeadersOptions()
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            });
 
             app.UseHttpsRedirection();
 
