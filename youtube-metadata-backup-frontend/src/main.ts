07c9ac15e98550a222bcd88a32e1bb01e0f7303f
Shangqing Zong <yin2690855381@gmail.com>
2021-04-26 19:22:53 -0500

    Added code to set default youtube api key

--- youtube-metadata-backup-frontend/src/main.ts
+++ youtube-metadata-backup-frontend/src/main.ts
@@ -1,7 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import {Utils} from "@/assets/ts/Utils";
 
 Vue.config.productionTip = false
+
+if (!Utils.GetCookie("apiKey"))
+{
+  Utils.SetCookie("apiKey", "");
+}
 // @ts-ignore
 window.apiEndpointDomain = "https://localhost:44371";
 
