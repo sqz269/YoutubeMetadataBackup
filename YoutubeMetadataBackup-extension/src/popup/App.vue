7571e0ce8b5f334bb675eb913afd2bf32e8c6f8e
sqz269 <yin2690855381@gmail.com>
2021-10-15 13:55:36 -0400

    Added template for chrome extension
    template from: https://github.com/TakahashiShuuhei/chrome-extension-vue-typescript

---
+++ YoutubeMetadataBackup-extension/src/popup/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Dev Tools is no more. This Is Popup! {{ now }} </h1>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default class App extends Vue {
+  get now() {
+    return new Date();
+  }
+}
+</script>
