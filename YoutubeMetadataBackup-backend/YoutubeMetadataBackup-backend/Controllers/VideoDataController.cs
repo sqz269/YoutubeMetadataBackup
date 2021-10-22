9b40e1f1b80f7abebcacb5e92d82785384b7a676
sqz269 <yin2690855381@gmail.com>
2021-10-22 14:25:17 -0400

    Added fetch by video id api back

---
+++ YoutubeMetadataBackup-backend/YoutubeMetadataBackup-backend/Controllers/VideoDataController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using YoutubeMetadataBackup_backend.Models.api;
+using YoutubeMetadataBackup_backend.Services;
+
+namespace YoutubeMetadataBackup_backend.Controllers
+{
+    [EnableCors("AllowAll")]
+    [Route("api/youtube/videos/data/get")]
+    [ApiController]
+    public class VideoDataController
+    {
+        private readonly VideoService _videoService;
+
+        public VideoDataController(VideoService videoService)
+        {
+            _videoService = videoService;
+        }
+
+        [HttpGet]
+        [HttpPost]
+        public ExecutionResult<ListVideoResult> GetVideosFromId(IEnumerable<string> videoId)
+        {
+            var videoIds = videoId as string[] ?? videoId.ToArray();
+            var videoData = _videoService.Get(videoIds);
+            var items = videoData.Select(v => v.Id).ToArray();
+            var missingItems = videoIds.Except(items).ToArray();
+            return ExecutionResult<ListVideoResult>.Success(new ListVideoResult
+            {
+                videos = videoData,
+                noRecord = missingItems
+            }, 1);
+        }
+    }
+}
