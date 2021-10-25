3d68f7d35fb3d8c90e34e51ef56f1fd08a6e0b6a
sqz269 <yin2690855381@gmail.com>
2021-10-25 01:00:13 -0400

    Removed vuetify from extension

--- YoutubeMetadataBackup-extension/src/popup/App.vue
+++ YoutubeMetadataBackup-extension/src/popup/App.vue
@@ -1,15 +1,15 @@
 <template>
   <div>
-    <h1>Dev Tools is no more. This Is Popup! {{ now }} </h1>
+    <h1>This Is Popup! {{ now }} </h1>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
-
-export default class App extends Vue {
-  get now() {
-    return new Date();
+export default {
+  computed: {
+    now: () => {
+      return new Date();
+    }
   }
 }
 </script>
--- YoutubeMetadataBackup-extension/webpack.config.js
+++ YoutubeMetadataBackup-extension/webpack.config.js
@@ -52,29 +52,7 @@ module.exports = {
         options: {
           appendTsSuffixTo: [/\.vue$/],
         }
-      },
-      {
-        test: /\.s(c|a)ss$/,
-        use: [
-          'vue-style-loader',
-          'css-loader',
-          {
-            loader: 'sass-loader',
-            // Requires sass-loader@^7.0.0
-            options: {
-              implementation: require('sass'),
-              indentedSyntax: true // optional
-            },
-            // Requires >= sass-loader@^8.0.0
-            options: {
-              implementation: require('sass'),
-              sassOptions: {
-                indentedSyntax: true // optional
-              },
-            },
-          },
-        ],
-      },
+      }
     ]
   },
   plugins: [
